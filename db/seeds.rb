46cc60bae37e9cd5117a9aa885c55ee60cd990d2
vidya927 <vidya.bommanapally09@gmail.com>
2016-11-20 02:27:36 +0000

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Inventory.create(item_name: 'onion', quantity_available: 90)
+Inventory.create(item_name: 'chilli', quantity_available: 50)
+Inventory.create(item_name: 'salt', quantity_available: 60)
+Inventory.create(item_name: 'tomato', quantity_available: 40)
+Inventory.create(item_name: 'spices', quantity_available: 20)
+Inventory.create(item_name: 'chicken', quantity_available: 30)
+Inventory.create(item_name: 'noodles', quantity_available: 60)
+Inventory.create(item_name: 'eggs', quantity_available: 150)
+Inventory.create(item_name: 'brinjal', quantity_available: 90)
+Inventory.create(item_name: 'yogurt', quantity_available: 20)
+Inventory.create(item_name: 'capsicum', quantity_available: 50)
+Inventory.create(item_name: 'pepper', quantity_available: 20)
+Inventory.create(item_name: 'spinach', quantity_available: 30)
+Inventory.create(item_name: 'cheese', quantity_available: 20)
+Inventory.create(item_name: 'olive oil', quantity_available: 100)
+Inventory.create(item_name: 'sause', quantity_available: 90)
+Inventory.create(item_name: 'peas', quantity_available: 40)
+Inventory.create(item_name: 'broccoli', quantity_available: 78)
+Inventory.create(item_name: 'cauliflower', quantity_available: 76)
+Inventory.create(item_name: 'carrot', quantity_available: 66)
+Inventory.create(item_name: 'rice', quantity_available: 150)
+Inventory.create(item_name: 'gulab jamun flour', quantity_available: 82)
+Inventory.create(item_name: 'sugar', quantity_available: 82)
+Inventory.create(item_name: 'corn flour', quantity_available: 94)
+Inventory.create(item_name: 'ghee', quantity_available: 82)
+
+Recipe.create(recipe: 'spicy noodles', ingredients: 'noodles',required_qty_in_gm:'6')
+Recipe.create(recipe: 'spicy noodles', ingredients: 'spices',required_qty_in_gm:'1')
+Recipe.create(recipe: 'spicy noodles', ingredients: 'tomato',required_qty_in_gm:'3')
+Recipe.create(recipe: 'spicy noodles', ingredients: 'capsicum',required_qty_in_gm:'2')
+Recipe.create(recipe: 'spicy noodles', ingredients: 'chilli',required_qty_in_gm:'1')
+Recipe.create(recipe: 'spicy noodles', ingredients: 'pepper',required_qty_in_gm:'1')
+Recipe.create(recipe: 'spicy noodles', ingredients: 'cauliflower',required_qty_in_gm:'2')
+
+Recipe.create(recipe: 'fried rice', ingredients: 'rice',required_qty_in_gm:'7')
+Recipe.create(recipe: 'fried rice', ingredients: 'spices',required_qty_in_gm:'2')
+Recipe.create(recipe: 'fried rice', ingredients: 'tomato',required_qty_in_gm:'4')
+Recipe.create(recipe: 'fried rice', ingredients: 'capsicum',required_qty_in_gm:'2')
+Recipe.create(recipe: 'fried rice', ingredients: 'chilli',required_qty_in_gm:'1')
+Recipe.create(recipe: 'fried rice', ingredients: 'pepper',required_qty_in_gm:'1')
+Recipe.create(recipe: 'fried rice', ingredients: 'cauliflower',required_qty_in_gm:'2')
+
+Recipe.create(recipe: 'gulab jamun', ingredients: 'gulab jamun',required_qty_in_gm:'5')
+Recipe.create(recipe: 'gulab jamun', ingredients: 'sugar',required_qty_in_gm:'2')
+Recipe.create(recipe: 'gulab jamun', ingredients: 'ghee',required_qty_in_gm:'4')
+
+Order.create(customer_name: 'jack', ordered_dish: 'fried rice', Quntatity_of_each_item:'1', no: '2')
+Order.create(customer_name: 'john', ordered_dish: 'gulab jamun', Quntatity_of_each_item:'2', no: '2')
+
+
+
+
+
+
+
