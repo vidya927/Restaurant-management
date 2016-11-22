ff2dfbbe643c5c36f02c2542b5671eba39b5aa90
vidya927 <vidya.bommanapally09@gmail.com>
2016-11-22 03:30:30 +0000

    checkout

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120012312) do
+ActiveRecord::Schema.define(version: 20161121125956) do
 
   create_table "catalogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "chefs", force: :cascade do |t|
+    t.string   "customer_name"
+    t.string   "ordered_dish"
+    t.float    "quantity"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "dish_name"
+    t.float    "price"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "inventories", force: :cascade do |t|
@@ -48,6 +64,13 @@ ActiveRecord::Schema.define(version: 20161120012312) do
     t.integer  "required_qty_in_gm"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "temps", force: :cascade do |t|
+    t.string   "ingredients"
+    t.float    "qty"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
